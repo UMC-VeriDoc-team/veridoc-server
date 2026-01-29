27ecfd45411dd0bca1bb113d7a0c743a9336f8c0
Daniel Kim <jukhap2@gmail.com>
2026-01-29 14:57:38 +0900

    마스터 jwt 토큰 발급 API 구현 및 유저 API에 미들웨어 적용

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import dotenv from "dotenv";
 import express from 'express';
 import userRoutes from './user.routes.js';
 import painAreaRoutes from './painArea.routes.js';
+import adminRoutes from './admin.routes.js';
 
 
 const router = express.Router();
 
 router.use('/users', userRoutes);
 router.use('/pain-areas', painAreaRoutes);
+router.use('/admin', adminRoutes);
 
 export default router;
