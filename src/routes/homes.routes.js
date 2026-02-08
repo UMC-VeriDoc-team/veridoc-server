6d0086a66fab1816b7f40ae9d242386ef981517f
ashB1124 <rnbashb1124@gmail.com>
2026-02-09 02:58:53 +0900

    WIP

--- src/routes/homes.routes.js
+++ src/routes/homes.routes.js
@@ -5,11 +5,14 @@ import { authenticate } from '../middleware/auth.middleware.js';
 const router = express.Router();
 const controller = new HomeController();
 
+// /homes
 // 인증 필요한 라우트
 router.get('/', authenticate, controller.getHome);
+
+// /expert-answers
 // 전문의 답변 요약본
-router.get('/answers/:answerId/summary', authenticate, controller.getDoctorAnswerSummary);
+router.get('/:answerId/summary', authenticate, controller.getDoctorAnswerSummary);
 // 전문의 답변 상세
-router.get('/answers/:answerId', authenticate, controller.getDoctorAnswerDetail);
+router.get('/:answerId', authenticate, controller.getDoctorAnswerDetail);
 
 export default router;
